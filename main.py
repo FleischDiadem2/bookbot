37c220198efa6eeb5716389221ac4d6b5b05abdd
FleischDiadem2 <lucaslangmueller+github@gmail.com>
2025-04-12 13:13:40 +0200

    Finished Bookbot

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import sys
 from stats import return_sorted_count, count_characters, count_words, get_book_text
 
 def main():
-    book = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book = sys.argv[1]
+    
     #text = get_book_text("books/frankenstein.txt")
     #num_words = count_words("books/frankenstein.txt")
     #print(f"{num_words} words found in the document")
